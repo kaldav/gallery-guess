772a0edfbd0cbad1856ba91b1c582a27e834c53c
David Kalacska <david.kalacska@instructure.com>
2025-01-06 08:54:26 +0100

    fix things

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export', //static site generation
   trailingSlash: true,
-  /* config options here */
+  images: {
+    unoptimized: true,
+}
 };
 
 export default nextConfig;
