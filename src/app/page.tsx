05465604784c1275189cdda4f424ab68be05c5d4
David Kalacska <david.kalacska@instructure.com>
2025-01-06 09:17:56 +0100

    create game.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,17 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Hello
+        <h1 className="text-4xl font-bold">Gallery Guess</h1>
+        <p className="text-lg text-center sm:text-left">
+          Get ready to test your knowledge of art history! In this game, you'll
+          be shown a series of paintings and asked to identify the artist.
+        </p>
+        <a
+          className="p-4 text-white bg-black rounded-md hover:bg-gray-800"
+          href="/game"
+        >
+          Start Game
+        </a> 
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
