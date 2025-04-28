82c87de3b6314501a57f3bec75127a28fb7fa026
David Kalacska <david.kalacska@instructure.com>
2025-04-28 16:07:56 +0200

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Assuming your repository name will be "gallery-guess"
+  // Change this if your repository has a different name
+  basePath: process.env.NODE_ENV === 'production' ? '/gallery-guess' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
